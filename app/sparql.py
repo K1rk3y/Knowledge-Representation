5c94ee6b65f1a93ca486653e2b698818b2469731
K1rk3y <kirkadmiralsfc@gmail.com>
2024-10-08 15:15:27 +0800

    updated query

--- app/sparql.py
+++ app/sparql.py
@@ -37,26 +37,48 @@ SELECT ?item (COUNT(?procedure) AS ?procedure_count)
 WHERE {
     ?procedure rdf:type onto:Procedure.
     {
-        ?item rdf:type onto:Item.
+        
         ?procedure onto:isForItem ?item.
     } 
     UNION
     {
         ?pItem rdf:type onto:Item.
-        ?item rdf:type onto:Part.
+        
         ?item onto:isPartOf ?pItem.
         ?procedure onto:isForItem ?pItem.
     }
     UNION
     {
         ?item rdf:type onto:Item.
-        ?pItem rdf:type onto:Part.
+        
         ?pItem onto:isPartOf ?item.
         ?procedure onto:isForItem ?pItem.
     }
 }
 GROUP BY ?item
 HAVING (COUNT(?procedure) > 10)
+"""
+
+qres = g.query(query)
+for row in qres:
+    print(row)
+
+
+# Find all procedures that include a tool that is never mentioned in the procedure steps
+query = """
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX onto: <http://cits3005.org/pc-ontology.owl#>
+
+SELECT ?procedure ?tool WHERE {
+  ?toolbox onto:isForProcedure ?procedure.
+  ?toolbox onto:containsTool ?tool.
+  FILTER NOT EXISTS {
+    ?step onto:usesTool ?tool.
+    ?step onto:isPartOfProcedure ?procedure.
+  }
+}
 """
 
 qres = g.query(query)
